90f72a94138755089f434e13f7eee1e2737660bb
Kieran Murray <kam597@york.ac.uk>
2024-07-18 18:13:59 +0100

    Updated Comments
    Just check we can do this by using export

---
+++ Calculation_Numeric.py
@@ -0,0 +1,119 @@
+from ReducedCubicEquation_InitialGuess import ReducedCubicEquation_InitialGuess
+from ReducedCubicEquation_NewtonRaphson import ReducedCubicEquation_NewtonRaphson
+
+
+import math
+
+'''
+This program solves a reduced cubic eqaation such as s**3 + px = q(x) = 0 where we supply the parameters
+for p and q. We sue a lambda function within a lambda function to do this
+'''
+
+BoltzmannConstant= 1.38 * 10**(-23)
+ElectricalCharge= 1.602 * 10**(-19)
+#NumberDensitySOLMid =  10**19
+#ParallelHeatFlux = 5 * 10**6
+MassDeuterium = 3.3435 * 10**(-27)
+Gamma = 7.5 
+#TokamakMajorRadius = 9 # This is a very large tokamak
+#TokamakSafetyFactor = 3 # This is a large safety factor
+ElectronParallelConductivityCoefficient=2000 # Common value that is used
+
+
+
+
+class Calculation_Numeric():
+
+
+    def __init__(self):
+        #print('Hello World')
+        pass
+    '''
+    def fmomentumLoss(self, x):
+           Ttarget =5
+           n = self.momentumfactor
+           #print('This is n', n)
+           Tnorm = x/Ttarget
+           if Tnorm >= 1:
+               return 1
+           else:
+               return Tnorm**n 
+     '''      
+           
+    def fmomentumLoss(self, x):
+          Ttarget =1
+          n = self.momentumfactor
+          #print('This is n', n)
+          Tnorm = x/Ttarget
+          return (1-(math.exp(-Tnorm)))**n
+           
+    
+           
+    def Calculate(self, A, B, C, D, E, F, G):
+        '''
+        
+        This function/method obtains the independent variable from a reduced cubic equation.
+        The parameters for the reduced cubic equation are a, p and q i,e ax**3 + px + q 
+        These are the values obtained by calculation in the first part of the method below
+        '''
+        #print('HelloAgain')
+        try:
+            NumberDensitySOLMid = A # not sure why you have to use *args for this to work
+            ParallelHeatFlux = B # not sure why you have to use *args for this to work
+            fpowerLoss = C
+            self.momentumfactor = D
+            #print('This is the momentum factor', self.momentumfactor)
+            fconductionLoss = E
+            TokamakMajorRadius = F
+            TokamakSafetyFactor = G
+            ConnectionLength =  math.pi * TokamakMajorRadius * TokamakSafetyFactor
+            p = 3.5 * ParallelHeatFlux * ConnectionLength *fconductionLoss /ElectronParallelConductivityCoefficient
+           # print(p)
+            Beta = math.sqrt(2)* ElectricalCharge**(3.0/2)/math.sqrt(MassDeuterium)
+           # print(Beta)
+            q = lambda x: (2*ParallelHeatFlux*fpowerLoss/(Beta* NumberDensitySOLMid * self.fmomentumLoss((x**(4.0/7)))))**(7.0/2)
+            from scipy.optimize import fsolve
+            tosolve = lambda x: x**3 + p*x - q(x)
+            #print(q)
+            self.TemperatureTarget = fsolve(tosolve,1**(7.0/4))**(4.0/7)
+            #self.TemperatureTarget = fsolve(tosolve,x0=1)
+            print(self.TemperatureTarget)
+            '''
+            a=1
+            M= ReducedCubicEquation_InitialGuess(a,p,q(5**(7.0/4)))
+            # The initial guess just involves coming up with a high and low value so we have bounds to apply the Newton Raphson
+           # print('This is the reduced')
+            #print(M.xlow)
+            #print(M.xhigh)
+            
+            xlowhere= abs(M.xlow)
+            xhighhere= abs(M.xhigh)
+            
+            P= ReducedCubicEquation_NewtonRaphson(xlowhere, xhighhere, a,p,q)
+            self.TemperatureTarget = (P.xnew)**(4.0/7)
+            
+            
+            '''
+            TemperatureMidPontFirstPart = self.TemperatureTarget**(7.0/2) + p
+            
+            self.TemperatureMidPoint =  TemperatureMidPontFirstPart**(2.0/7)
+            
+            #print(self.TemperatureTarget) 
+            #print(self.TemperatureMidPoint)
+            self.NumberDensityTarget= self.fmomentumLoss(self.TemperatureTarget)*NumberDensitySOLMid * self.TemperatureMidPoint/(2 *  self.TemperatureTarget)
+          
+            #self.NumberDensityTarget=  NumberDensitySOLMid * self.TemperatureMidPoint/(2 * self.TemperatureTarget)
+            self.SOLMidPointPressure = NumberDensitySOLMid * self.TemperatureMidPoint
+            self.FluxAtTarget = self.NumberDensityTarget*math.sqrt(self.TemperatureTarget)* math.sqrt(2*ElectricalCharge/MassDeuterium)/self.SOLMidPointPressure
+            
+            self.TargetPressure = 2* self.NumberDensityTarget * self.TemperatureTarget
+            
+           
+           # NumberDensityAtTarget.set(NumberDensityTarget)
+           # print(self.NumberDensityTarget)
+        except ValueError:
+            pass
+    
+#app = Calculation_Numeric()
+#app.Calculate(10**20, 10**7, 0.1,1,1,3,4 ) 
+#print(app.TemperatureTarget) 
---
+++ InitialGui.py
@@ -0,0 +1,85 @@
+import customtkinter
+import tkinterDnD
+from tkinter import *
+
+from Calculation_Numeric import Calculation_Numeric
+from SliderGui import SliderGui
+
+
+'''
+This is the main program it sets up the initial Gui where the user is asked to input via drop-downs, the SOL Mid-point 
+density and the heat flux in W/m^3
+When the button 'Calculate values is clicked, the SliderGui program is insubstatiated and from this the plots are produced.
+There will be some documentation on this
+'''
+
+class InitialGui(customtkinter.CTk):
+    
+    def button_callback(self):
+        #print("Button click", self.optionmenu_1.get())
+        #print("Button click", self.optionmenu_2.get())
+        p = float(self.optionmenu_1.get());
+        g = float(self.optionmenu_2.get().replace(',', ''));
+        h = float(self.entry.get())
+        i = float(self.optionmenu_3.get());
+        print('This is p, g h and i', p, g, h, 'and', i)
+        S = SliderGui()
+        S.runplot(p,g, h, i)
+        
+    
+    def __init__(self):
+        #customtkinter.set_ctk_parent_class(tkinterDnD.Tk)
+        
+        #customtkinter.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
+        #customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+        
+        super().__init__()
+        self.geometry("1000x480")
+        self.title("CustomTkinter simple_example.py")
+        
+       # print(type(app), isinstance(app, tkinterDnD.Tk))
+        
+        self.TempAtTarget = StringVar()
+        self.TempAtSolMidPoint = StringVar()
+        self.NumberDensityAtTarget=StringVar()
+        
+        
+        frame_1 = customtkinter.CTkFrame(master=self)
+        frame_1.pack(pady=20, padx=60, fill="both", expand=True)
+        
+        label_1 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT, text = "Please enter the Electron number density at the SOL Mid=point")
+        label_1.grid(row=1, column=0, padx=20, pady=20)
+        
+        label_2 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT, text = "Please enter the heat flux at the target in W/$m^2$")
+        label_2.grid(row=3, column=0, padx=20, pady=20)
+        
+        label_3 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT, text = "Please enter the major radius of the tokamak in metres")
+        label_3.grid(row=4, column=0, padx=20, pady=20)
+        
+        label_4 = customtkinter.CTkLabel(master=frame_1, justify=customtkinter.LEFT, text = "Please enter the Safety Value")
+        label_4.grid(row=5, column=0, padx=20, pady=20)
+        
+        button_1 = customtkinter.CTkButton(master=frame_1, command=self.button_callback, text = 'Calculate Values')
+        button_1.grid(row=6, column=1, padx=20, pady=20)
+        
+        
+        
+        
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(frame_1,values=["1e17", "1e18", "1e19", "1e20", "1e21"])
+        self.optionmenu_1.grid(row=1, column=1, padx=20, pady=20)
+        self.optionmenu_1.set("Electron_Number_Density_OptionMenu")
+        
+        self.optionmenu_2 = customtkinter.CTkOptionMenu(frame_1, values=["10,000","100,000", "1,000,000", "10,000,000"])
+        self.optionmenu_2.grid(row=3, column=1, padx=20, pady=20)
+        self.optionmenu_2.set("Heat_Flux_At_Target_Watts_metresquared_OptionMenu")
+        
+        self.entry = customtkinter.CTkEntry(frame_1, placeholder_text="CTkEntry")
+        self.entry.grid(row=4, column=1, padx=20, pady=20)
+        
+        self.optionmenu_3 = customtkinter.CTkOptionMenu(frame_1, values=["1","2","3", "4", "5", "6"])
+        self.optionmenu_3.grid(row=5, column=1, padx=20, pady=20)
+        self.optionmenu_3.set("Safety Value of tokamak ")
+        
+        
+app = InitialGui()       
+app.mainloop()
